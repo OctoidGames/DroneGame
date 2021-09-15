47699c7a807ff6c9941324af9359e86a962d22f2
OctoidGames <87039261+OctoidGames@users.noreply.github.com>
2021-09-15 02:03:55 +0000

    Add files via upload

---
+++ models/colectable/CollectableRotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var oldRotation = transform.rotation;
+        transform.Rotate(Time.deltaTime * 90, Time.deltaTime*90, 0);
+        var newRotation = transform.rotation;
+
+            transform.rotation = Quaternion.Slerp(oldRotation, newRotation, 0);
+
+
+        transform.rotation = newRotation; // To make it come out at exactly 90 degrees
+    }
+}
